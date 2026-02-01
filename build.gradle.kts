a1b1b11bd8e02c39a072c05f6955ab0b1d21e6de
herbrine8403 <weishixvn@outlook.com>
2026-02-01 14:39:32 +0800

    Fix: Remove KotlinCompile reference from root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,3 @@ allprojects {
         gradlePluginPortal()
     }
 }
-
-subprojects {
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
-}
