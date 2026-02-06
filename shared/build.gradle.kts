f9d95154564261c6e842cb1f1df677704c1daef4
herbrine8403 <weishixvn@outlook.com>
2026-02-06 09:54:52 +0800

    Fix: Move cocoapods block inside kotlin block
    - Move cocoapods configuration from root level to inside kotlin block
    - This resolves the 'Unresolved reference' error on Linux CI runners
    - CocoaPods plugin is now properly configured within the Kotlin Multiplatform extension

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,6 +18,27 @@ kotlin {
     iosArm64()
     iosSimulatorArm64()
 
+    cocoapods {
+        version = "1.0.0"
+        summary = "LucidDreaming shared module"
+        homepage = "https://github.com/herbrine8403/LucidDreaming"
+        
+        ios.deploymentTarget = "15.0"
+        
+        framework {
+            baseName = "Shared"
+            
+            // Required by Kotlin/Native to work with Coroutines
+            export("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+            
+            // Required by Ktor
+            export("io.ktor:ktor-client-core:2.3.7")
+            export("io.ktor:ktor-client-content-negotiation:2.3.7")
+        }
+        
+        podfile = project.file("../iosApp/Podfile")
+    }
+
     sourceSets {
         commonMain {
             dependencies {
@@ -61,25 +82,4 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-}
-
-cocoapods {
-    version = "1.0.0"
-    summary = "LucidDreaming shared module"
-    homepage = "https://github.com/herbrine8403/LucidDreaming"
-    
-    ios.deploymentTarget = "15.0"
-    
-    framework {
-        baseName = "Shared"
-        
-        // Required by Kotlin/Native to work with Coroutines
-        export("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-        
-        // Required by Ktor
-        export("io.ktor:ktor-client-core:2.3.7")
-        export("io.ktor:ktor-client-content-negotiation:2.3.7")
-    }
-    
-    podfile = project.file("../iosApp/Podfile")
 }
